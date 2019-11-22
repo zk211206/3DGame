b3ff5cec3d9525efa9d910b2df6119add3b28b6a
zhang_kai35 <5254100+zhang_kai35@user.noreply.gitee.com>
2019-11-22 11:28:09 +0800

    HW8

---
+++ GUI/Assets/Scripts/IMGUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGUI : MonoBehaviour
+{
+        public float bloodValue;
+
+    private float tmpValue;
+
+    void Start()
+    {
+        tmpValue = 0.0f;
+        bloodValue = 0.0f;
+    }
+
+    void OnGUI()
+    {    
+        //点击加血
+        if (GUI.Button(new Rect(250, 20, 40, 20), "+"))
+        {
+            tmpValue = 1.0f;
+        }
+
+        //点击减血
+        if (GUI.Button(new Rect(250, 50, 40, 20), "-"))
+        {
+            tmpValue = 0.0f;
+        }
+
+        bloodValue = Mathf.Lerp(bloodValue, tmpValue, 0.01f);
+
+        GUI.color = Color.red; //血条，设置为红色
+        GUI.HorizontalScrollbar(new Rect(20, 20, 200, 20), 0.0f, bloodValue,0.0f, 1.0f, GUI.skin.GetStyle("HorizontalScrollbar"));
+
+    }
+}
---
+++ GUI/README.md
@@ -0,0 +1,3 @@
+# 视频链接
+
+https://www.bilibili.com/video/av76524620/
